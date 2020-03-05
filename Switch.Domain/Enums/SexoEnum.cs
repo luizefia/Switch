c7b258a1460a340a6ba0b976e4871cd57555a3f1
luizefia <luizefia@yahoo.com.br>
2020-03-05 17:43:41 -0300

    Criação de models

---
+++ Switch.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        Feminino = 1,
+        Masculino = 2,
+    }
+}
